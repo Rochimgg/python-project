9874e3d765b27d91a81df9aa1a59e0734de1c818
Ro Gallo <rgallo@medallia.com>
2020-05-17 19:50:28 -0300

    aniadidas diapositivas dispo

--- dispositivos_semiconductores/3_diodo_de_juntura_pn/guia_3_ej_1.py
+++ dispositivos_semiconductores/3_diodo_de_juntura_pn/guia_3_ej_1.py
@@ -6,11 +6,9 @@ from dispositivos_semiconductores.funciones_calculos import *
 def main():
     phi_b = 0.9 * unit.volt  # V
     print(phi_b)
-    R = 3 * unit["ohm"]
     Na = 1e16 * unit['1/centimeter**3']
     Nd = 1e15 * unit['1/centimeter**3']
-    current = phi_b/R
-    print(current)
+    Cj0_a = 0 * unit['farad']
 
 
 if __name__ == '__main__':
